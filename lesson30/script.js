88eba9d986dadd468d5146e080dfe7bce6c099e4
Saba Okropiridze <sabaoqropiridze2003@gmail.com>
2026-08-10 17:43:06 +0400

    new  lesson

--- lesson30/script.js
+++ lesson30/script.js
@@ -1,1 +1,16 @@
-const button = document.getElementById('button')
+const button = document.getElementById('button');
+const course = document.getElementById('course');
+const student = document.getElementById('student');
+
+let isOriginal = true;
+
+button.addEventListener('click', function () {
+  if (isOriginal) {
+    course.innerText = 'javascript';
+    student.innerText = 'davit';
+  } else {
+    course.innerText = 'Python';
+    student.innerText = 'otar';
+  }
+  isOriginal = !isOriginal; // Switches the toggle state
+});

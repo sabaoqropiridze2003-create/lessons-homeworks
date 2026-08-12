798ba3ebe0035ec7ddad5e9ee3c3314e3ac40ab5
Saba Okropiridze <sabaoqropiridze2003@gmail.com>
2026-08-12 21:33:06 +0400

    finished lecture 31

--- lesson31/lecture31.py
+++ lesson31/lecture31.py
@@ -155,6 +155,122 @@ import streamlit as st
 #     st.header("right one")
 #     st.write("coment number 3")
 
+# text = st.expander("details")
+# with text:
+#     st.write("this text apears and dissapears")
+#     st.code("print('hello')", language="python")
 
-with st.expander("details"):
-    st.write("this text apears and dissapears")
+# page = st.sidebar.radio(
+#     "navigation", ["main page", "about info", "contuct us"])
+
+# if page == "main page":
+#     st.title("This is main page")
+#     st.write("Welcome to this site")
+# elif page == "about info":
+#     st.title("info about us")
+# elif page == "contuct us":
+#     st.title("contacts")
+
+
+# st.sidebar.title("parameters")
+# name = st.sidebar.text_input("name")
+# age = st.sidebar.slider("age", 0, 120, 22)
+
+# with st.sidebar:
+#     st.title("parameters")
+#     st.slider("age", 0, 100, 22)
+#     option = st.selectbox("oprions", ["A", "B"])
+
+
+# tab1, tab2 = st.tabs(["data", "graph"])
+
+# with tab1:
+#     st.title("first tab")
+# with tab2:
+#     st.title("second tab")
+
+# students = [
+#     {"სახელი": "ნინო", "ასაკი": 22, "ქულა": 95},
+#     {"სახელი": "გიორგი", "ასაკი": 24, "ქულა": 88},
+#     {"სახელი": "ანა", "ასაკი": 21, "ქულა": 92},
+#     {"სახელი": "დავითი", "ასაკი": 23, "ქულა": 78},
+# ]
+# st.table(students)
+# st.dataframe(students)
+
+# col1, col2, col3 = st.columns(3)
+
+# with col1:
+#     st.metric("temperature", "23 C", "2 C", "inverse")
+# with col2:
+#     st.metric("income", "12000$", "-500$")
+# with col3:
+#     st.metric("costumer", 1234, "12%")
+
+# # with st.sidebar:
+# #     st.metric("temperature", "23 C", "2 C", "inverse")
+# #     st.metric("income", "12000$", "-500$")
+# #     st.metric("costumer", 1234, "12%")
+
+
+# chart_data = {
+#     "product A": [100, 120, 300, 400, 30, 60],
+#     "product B": [80, 90, 110, 40, 120, 300],
+#     "product c": [180, 290, 210, 140, 120, 30]
+# }
+
+# st.line_chart(chart_data)
+
+
+# import random
+
+# data = {
+#     "value": [random.randint(10, 100) for _ in range(20)]
+# }
+# st.area_chart(data)
+
+# data = [random.randint(10, 100) for _ in range(20)]
+
+# st.area_chart(data)
+
+# data = [1, 4, 5, 2, 23]
+# st.area_chart(data)
+
+# data = {
+#     "value": [1, 10, 23, 24, 12, 10]
+# }
+
+# st.area_chart(data)
+
+
+# count = 0
+
+# if st.button("add"):
+#     count += 1
+#     st.write(f"counter: {count}")
+
+
+# if "count" not in st.session_state:
+#     st.session_state.count = 0
+
+# if st.button("add"):
+#     st.session_state.count += 1
+#     st.write(f"counter: {st.session_state.count}")
+
+
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if st.button("add"):
+        st.session_state.count += 1
+with col2:
+    if st.button("subtracs"):
+        st.session_state.count -= 1
+
+with col3:
+    if st.button("null"):
+        st.session_state.count = 0
+st.write(f"counter: {st.session_state.count}")

61dbb5549b99f0f4168790fe490aef36e5d3676a
saba <sabaoqropiridze2003@gmail.com>
2026-09-03 00:06:00 +0400

    updated

--- lesson37/lecture37.py
+++ lesson37/lecture37.py
@@ -5,3 +5,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "world"}
+
+@app.get("/info")
+def read_info():
+    return {"name": "my first app",
+            "version": "1.0",
+            "description": "my first app"
+            }
+
+@app.get("/users")
+def read_users():
+    return [{"id":1, "name": "girogi", "grade":100},
+            {"id":2, "name": "saba", "grade":78},
+            {"id":3, "name": "anano", "grade":86}
+            ]

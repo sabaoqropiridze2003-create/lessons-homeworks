11577ae5c8dceeb261c38ce0dfb5dd5a4dd1740a
saba <sabaoqropiridze2003@gmail.com>
2026-09-17 17:10:09 +0400

    lesson42

---
+++ lesson42/app/models/category.py
@@ -0,0 +1,14 @@
+from app.database import Base
+from sqlalchemy import String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    products: Mapped[list["Product"]] = relationship(back_populates="category", cascade="all, delete")
+
+    def __str__(self):
+        return self.name
+
---
+++ lesson42/app/models/product.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Boolean, ForeignKey, Integer, String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base
+from datetime import datetime
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(255))
+    price: Mapped[float] = mapped_column(Integer)
+    quantity: Mapped[int] = mapped_column(Integer)
+    is_available: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id", ondelete="CASCADE"))
+    category: Mapped["Category"] = relationship(back_populates="products")

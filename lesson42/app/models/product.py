67d59a57cf3832fe755f8c3d7cde6ac81e128b53
saba <sabaoqropiridze2003@gmail.com>
2026-09-18 22:24:26 +0400

    fixed some stuff

--- lesson42/app/models/product.py
+++ lesson42/app/models/product.py
@@ -10,7 +10,7 @@ class Product(Base):
     name: Mapped[str] = mapped_column(String(100))
     price: Mapped[float] = mapped_column(Integer)
     quantity: Mapped[int] = mapped_column(Integer)
-    is_available: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_available: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
